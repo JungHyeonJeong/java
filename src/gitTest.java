6c83a304168d9d2769d6eb57fae73efacd060e2c
hsol19 <kimhgst919@naver.com>
2022-08-20 11:17:19 +0900

    git second

--- src/gitTest.java
+++ src/gitTest.java
@@ -1,5 +1,10 @@
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("git Test");
+
+        System.out.println("Test1 - git");
+        System.out.println("Test1 - git");
+        System.out.println("Test1 - git");
+
     }
 }
